b9d6d4d235a9333e068c85c0fbf69aa5bd6e747c
RayDream3 <155059820+RayDream3@users.noreply.github.com>
2023-12-31 20:45:44 +0800

    Update style.js

--- themes/hexo/style.js
+++ themes/hexo/style.js
@@ -8,7 +8,7 @@ const Style = () => {
   return (<style jsx global>{`
     // 底色
     body{
-        background-color: #f5f5f5;
+        background-color: #222;
         cursor: url("../public/normal.cur"), default;
     }
     .dark body{
