be5ce59df7dac52ec1c64c38396e6262672956a6
RayDream3 <155059820+RayDream3@users.noreply.github.com>
2023-12-31 22:02:21 +0800

    Update Hero.js

--- themes/hexo/components/Hero.js
+++ themes/hexo/components/Hero.js
@@ -20,7 +20,7 @@ const Hero = props => {
   const scrollToWrapper = () => {
     window.scrollTo({ top: wrapperTop, behavior: 'smooth' })
   }
-  #const GREETING_WORDS = siteConfig('GREETING_WORDS').split(',')
+  //const GREETING_WORDS = siteConfig('GREETING_WORDS').split(',')
   const GREETING_WORDS = await fetch('https://v1.hitokoto.cn/?c=f&encode=text')
   useEffect(() => {
     updateHeaderHeight()
